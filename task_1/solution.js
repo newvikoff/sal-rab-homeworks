b4d9104e14a607503b55e092ca87ba4a9fdfbce5
newvikoff <neeregistr@yandex.ru>
2021-12-04 20:51:07 +0300

    Домашнее хадание 1.1, 1.2, 1.3

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,7 +1,16 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
+    let message;
+    message = productName;
+    message = message + " ";
+    message = message + "за";
+    message = message + " ";
+    message = message + productPrice;
+    message = message + " ";
+    message = message + "теперь в корзине!";
+    
+    
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
@@ -23,7 +32,9 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
+    let newValue;
+    newValue = oldValue;
+    newValue = newValue + 1;
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -42,7 +53,10 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText;
+    newSumText = newSum + ' ₽'
     // Конец решения задания №1.3.
 
     return newSumText;
